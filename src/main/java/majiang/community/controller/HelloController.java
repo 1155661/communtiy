633bc08fc7e59440ff155c16cc6a75c0310278e3
mo <1468033693@qq.com>
2019-05-28 09:52:21 +0800

    first commit

---
+++ src/main/java/majiang/community/controller/HelloController.java
@@ -0,0 +1,34 @@
+package majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * * +----------------------------------------------------------------------
+ * * | 广西西途比网络科技有限公司 www.c2b666.com
+ * * +----------------------------------------------------------------------
+ * * | 功能描述: 请输入描述
+ * * +----------------------------------------------------------------------
+ * * | 时　　间: 2019/5/28 9:27
+ * * +----------------------------------------------------------------------
+ * * | 代码创建: 莫祖能 <1468033693@qq.com>
+ * * +----------------------------------------------------------------------
+ * * | 版本信息: V1.0.0
+ * * +----------------------------------------------------------------------
+ * * | 代码修改:（修改人 - 修改时间）
+ * * +----------------------------------------------------------------------
+ **/
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+
+        model.addAttribute("name",name);
+
+        return "hello";
+    }
+
+}
